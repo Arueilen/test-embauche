2fdca60ca8a78de67a857e1d7efc138162038e85
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2018-05-18 16:24:36 +0200

    select.openearch+.closeSearch finish

--- modules/select.js
+++ modules/select.js
@@ -2,16 +2,41 @@ const knex_owners = require('knex')(require('../knex/knexfile.owners')),
 knex_dogs = require('knex')(require('../knex/knexfile.dogs'));
 
 module.exports = {
-	closeSearchDog : (query) => {
-		// call knex to select all dogs where owners_id is the selected owner's id
-		return knex_dogs.select('id', 'name', 'owner_id').from('dog').where(query);
+	SearchDog : (owners) => {
+		return new Promise((resolve, reject)=>{
+
+			// get all owners' ids
+			let owner_ids = [];
+			owners.map(e=>{
+				owner_ids.push(e.id);
+			});
+
+			// call knex to select all dogs where owner_id is in the selected owner_ids
+			knex_dogs.select('id', 'name', 'owner_id').from('dog').whereIn('owner_id', owner_ids)
+			.then(dogs=>{
+				owners.map(owner=>{
+					owner.dogs = dogs.filter(dog=> dog.owner_id == owner.id)
+				});
+				resolve(owners);
+			})
+		})
 	},
 	openSearch : (query) => {
-		// select all owners where query's ok
-		return knex_owners.select('id', 'last_name', 'first_name', 'civility', 'age').from('owner').where(query);
+
+		queryKeys = Object.keys(query);
+
+		// select all owners
+		r = knex_owners.select('id', 'last_name', 'first_name', 'civility', 'age').from('owner')
+
+		queryKeys.map(e=>{
+			// for each query search all owners where a collumn is named as the key and set as the value
+			r = r.orWhere(e, query[e]);
+		})
+
+		return r;
 	},
 	closeSearch : (query) => {
-		// select all owners where query's ok
-		return knex_owners.select('id', 'last_name', 'first_name', 'civility', 'age').from('owner').where(query)
+		// select all owners where query's exact
+		return knex_owners.select('id', 'last_name', 'first_name', 'civility', 'age').from('owner').where(query);
 	}
 }
--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-const mysql = require('mysql'),
-express = require('express'),
-select = require('./modules/select.js'),
-bodyParser = require('body-parser'),
+var mysql = require('mysql'),
+    express = require('express'),
+    select = require('./modules/select.js'),
+    bodyParser = require('body-parser'),
 
-app = express();
+    app = express();
 
 app.use(express.static('public'));
 // use bodyparser in req.body...
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-	res.sendFile('views/search.html', {root: __dirname });
+app.get('/', (req, res) =>{
+    // send an html form to send request to api (not functional, but the API is ok)
+    res.sendFile('views/search.html', {root: __dirname });
 });
 app.get('/closeSearch', (req, res)=>{
-	select.closeSearch(req.query)
-	.then((e)=>{
-		res.send(e);
-	});
+    // call closeSearch 
+    select.closeSearch(req.query)
+    .then(e=>{
+        select.SearchDog(e)
+        .then(r=>{
+            res.send(r);
+        })
+    });
 });
 app.get('/openSearch', (req, res)=>{
-	select.openSearch(req.query)
-	.then((e)=>{
-		res.send(e);
-	});
+    select.openSearch(req.query)
+    .then(e=>{
+        select.SearchDog(e)
+        .then(r=>{
+            res.send(r);
+        })
+    });
 });
 app.listen(8000, ()=>console.log('app running on localhost:8000'));
