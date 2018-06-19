e7544fecb0a4431ca6961bef51483c89965bdce8
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2018-06-19 20:22:45 +0200

    change request traitment for adapt to API server

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.use(express.static('public'));
 // use bodyparser in req.body...
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-	res.sendFile('views/ownerForm.html', {root: __dirname });
+app.get('/', (req, res) => {
+	res.send("les routes de l'api sont : <br>\
+        pour voir tout les proprio -> /owners <br>\
+        pour filtrer -> /owners?last_name=propriolastname&id=ownerid&ect.. <br>\
+        pour creer un owner -> /createOwner?name=plutot&owner_id=owner_id;"
+        );
 });
-app.get('/owners', (req, res)=>{
+app.get('/owners', (req, res) => {
 	select.allowners(req.query).then((e)=>{
 		res.send(e);
 	});
@@ -23,16 +27,21 @@ app.get('/owners', (req, res)=>{
 app.post('/createOwner', (req, res) => {
 	store.owner({
 		// passing props to store owner
-		first_name: req.body.first_name,
-		last_name: req.body.last_name,
-		civility: req.body.civility,
-		age: req.body.age
+		first_name: req.query.first_name,
+		last_name: req.query.last_name,
+		civility: req.query.civility,
+		age: req.query.age
 	})
 	// store owner being a Promise, on resolve send status 200
 	.then(() => (select.allowners(req.query).then((e)=>{
 		// res.redirect('/owners');
+		console.log('owner created: ', e)
 		res.json(e);
 	})))
+	.catch(function(error) {
+		console.log(error)
+		res.send(error)
+	})
 });
 
-app.listen(8080, ()=>console.log('app owners running on localhost:8080'));
+app.listen(8080, () => console.log('app owners running on localhost:8080'));
