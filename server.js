1992cd46b757a2c54fd587737f2d55aecfb8a27d
CyrilDenoyelle <cyril-denoyelle@hotmail.fr>
2018-06-19 20:18:47 +0200

    comment, change for request other servers insted of connect to DB

--- server.js
+++ server.js
@@ -3,45 +3,44 @@ const HOST = 'localhost';
 
 var mysql = require('mysql'),
     express = require('express'),
-    select = require('./modules/select.js'),
+    getApi = require('./modules/getApi.js'),
+    postApi = require('./modules/postApi.js'),
     bodyParser = require('body-parser'),
     request = require('request'),
 
     app = express();
 
-
-
 app.use(express.static('public'));
-// use bodyparser in req.body...
 app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
-    select.dogsApi(req, res)
-    .then(e=>{
-        res.send(e);
-    })
+    res.send("les routes de l'api sont : <br>\
+        pour voir tout les proprio -> /api <br>\
+        pour filtrer -> /api?last_name=propriolastname&id=ownerid&ect.. <br>\
+        pour creer un chien -> /post/dog?name=plutot&owner_id=owner_id <br>\
+        pour creer un owner -> /post/owner?name=plutot&owner_id=owner_id"
+        );
+});
+app.get('/api', (req, res) => {
+    let dogs, owners;
+    getApi.ownersApi(req, res)
+    .then(o=>{owners = o})
+    .then(()=>getApi.dogsApi())
+    .then(d=>{dogs = d})
+    .then(()=>getApi.apiSetup(owners, dogs))
+    .then(result=>res.send(result))
     // res.sendFile('views/search.html', {root: __dirname });
 });
-app.get('/closeSearch', (req, res)=>{
-    // call closeSearch 
-    select.closeSearch(req.query)
-    .then(e=>{
-        select.SearchDog(e)
-        .then(r=>{
-            res.send(r);
-        })
-    });
-});
-app.get('/openSearch', (req, res)=>{
-    select.openSearch(req.query)
-    .then(e=>{
-        select.SearchDog(e)
-        .then(r=>{
-            res.send(r);
-        })
-    });
-});
 
+app.get('/post/owner', (req, res) => {
+    postApi.createOwner(req, res)
+    .then(e=>res.send(e))
+})
+
+app.get('/post/dog', (req, res) => {
+    postApi.createDog(req, res)
+    .then(e=>res.send(e))
+})
 
 app.listen(PORT, HOST)
 console.log('app API running on localhost:8000');
